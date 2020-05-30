07627f0039a30f7db8adcf6992086e7c0df2dfae
EsraaElNaggar <esraamelnaggar@hotmail.com>
2020-05-30 02:10:39 +0200

    done for now

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -38,9 +38,9 @@ class NavBar extends Component {
                     <div className="container" style={{ display: "flex", justifyContent: "space-between" }}>
                         <nav className="navbar navbar-expand-lg navbar-dark   container" >
                             <a className="navbar-brand" href="/home" style={{ fontFamily: '"Pacifico", cursive', color: 'gold' }}>Bloggester</a>
-                            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                                 <span className="navbar-toggler-icon" />
-                            </button>
+                            </button> */}
                             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                                 <ul className="navbar-nav mr-auto">
                                     <li className="nav-item ">
